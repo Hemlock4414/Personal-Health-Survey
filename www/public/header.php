7d8c5004be7782dfb11659b026514c09ffb36d38
philipp-burri <burri.philipp13@gmail.com>
2024-07-03 11:06:33 +0200

    template(footer,header)

---
+++ www/public/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <link rel="stylesheet" href="header-footer.css">
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <div class="header">
+        <h1 class="header-title">VitaQuest</h1>
+        </div>
+    </header>
+  
+</body>
+</html>
+
--- www/public/index.php
+++ www/public/index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,14 +6,20 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PHP Include Exercise</title>
+  <link rel="stylesheet" href="styles.css">
+
+
 </head>
 
 <body>
 
-  <?php
+<?php
+  
+include 'header.php';
 
+include 'main.php';
 
+include 'footer.php';
 
   ?>
 
