faf039b3d5b392baf99e78a018e8027b3c08bca4
Hemlock4414 <antonio.marrara@gmx.ch>
2024-07-04 15:51:40 +0200

    start+footer

--- www/public/footer.php
+++ www/public/footer.php
@@ -1,23 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.css" rel="stylesheet">
-  <!-- Andere Meta-Tags und CSS-Links -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-  <title>PHP Include Footer</title>
-</head>
-
-<body>
-
- 
 
 
-
-<footer class="text-center text-lg-start bg-body-tertiary">
+ <footer class="text-center text-lg-start bg-body-tertiary">
 
   <section class="d-flex justify-content-center justify-content-lg-between p-4" style="background-color: #98ef82;">
 
@@ -94,10 +77,3 @@
 
 </footer>
 
-
-
-  
-
-</body>
-
-</html>
--- www/public/index.php
+++ www/public/index.php
@@ -19,7 +19,9 @@ if (str_contains($_SERVER['SCRIPT_NAME'], "index.php")) {
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <!-- MDBootstrap CSS -->
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.css" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.css" rel="stylesheet">
+  <!-- Andere Meta-Tags und CSS-Links -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <link rel="stylesheet" type="text/css" href="src/css/style.css">
   <title>StartPage</title>
 </head>
@@ -132,7 +134,9 @@ if (str_contains($_SERVER['SCRIPT_NAME'], "index.php")) {
     </div>
   </section>
 
-<footer></footer>
+<?php
+include 'footer.php';
+?>
 
 </body>
 
