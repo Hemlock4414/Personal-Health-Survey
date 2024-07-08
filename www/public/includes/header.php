10dfe082161677895353bc111be07dea96c0cd76
Hemlock4414 <antonio.marrara@gmx.ch>
2024-07-08 11:04:22 +0200

    updateVormittag

--- www/public/includes/header.php
+++ www/public/includes/header.php
@@ -6,14 +6,30 @@
    
     <link rel="stylesheet" type="text/css" href="src/css/header.css">
     <link rel="stylesheet" type="text/css" href="src/css/style.css">
-    
+    <style>
+    a{
+        text-decoration: none;
+        color: white;
+        cursor: pointer;
+    }
+    </style>
 </head>
 <body>
     <header>
         <div class="header">
-        <h1 class="header-title"><a href="index.php">VitaQuest</a></h1>
+        
+            <a href="index.php">   
+            <img src="src/img/footer-logo.png" width="50px" class="me-4" style="padding-bottom: 5px;">    
+            </a>
+            <h1 class="header-title"><a href="index.php" class="text-decoration-none text-white">VitaQuest</a></h1>
+            
+            
+            
         </div>
     </header>
+
+
+
   
 </body>
 </html>
--- www/public/umfrage.php
+++ www/public/umfrage.php
@@ -13,6 +13,10 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- MDBootstrap CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.css" rel="stylesheet">
+    <!-- Andere Meta-Tags und CSS-Links -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="src/css/style.css">
     <link rel="stylesheet" type="text/css" href="src/css/fragen.css">
 
